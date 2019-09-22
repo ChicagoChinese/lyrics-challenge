056bd9e17bd2186faf5bb7af69d42bc52275fb58
Feihong Hsu <feihong.hsu@gmail.com>
2019-09-21 21:25:07 -0500

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+try:
+  from secrets import NOTION_TOKEN
+except:
+  NOTION_TOKEN = ''
+
+MUSIC_DIR = '/Users/fhsu/Music/hanyu'
+
+CHALLENGE_TABLE = 'https://www.notion.so/chicagochinese/19367d1bc800405fbfa5698679eec62c?v=e838eab67c3a48d1b6d47c6fe11a1e1b'
