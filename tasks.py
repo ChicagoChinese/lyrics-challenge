2c1379d46849a58ae8e8cf4446f04a65e32938cb
Feihong Hsu <feihong.hsu@gmail.com>
2019-10-20 15:45:38 -0500

    Add short descriptions to tasks

--- tasks.py
+++ tasks.py
@@ -6,12 +6,18 @@ from invoke import task
 
 @task
 def challenge(ctx, title):
+  """
+  Generate challenge for paste into ChineseForums.com
+  """
   from challenge import generate_challenge
   generate_challenge(title)
 
 
 @task
 def clean(ctx):
+  """
+  Clean files
+  """
   from challenge import challenge_file, clip_range_file, clip_file
   for file_ in (challenge_file, clip_range_file, clip_file):
     if file_.exists():
@@ -23,11 +29,17 @@ def clean(ctx):
 
 @task
 def csv(ctx, url):
+  """
+  Generate lyrics CSV file for import into Notion
+  """
   from generate import generate_lyrics_csv_file
   generate_lyrics_csv_file(url)
 
 
 @task
 def lyrics(ctx, title):
+  """
+  Add lyrics into Notion
+  """
   from challenge import add_lyrics
   add_lyrics(title)
