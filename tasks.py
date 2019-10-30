28f729d2fd773284acf6e9e3da58b240466650fe
Feihong Hsu <feihong.hsu@gmail.com>
2019-10-30 12:00:55 -0500

    Fix comment for csv task

--- tasks.py
+++ tasks.py
@@ -6,40 +6,40 @@ from invoke import task
 
 @task
 def challenge(ctx, url):
-  """
-  Generate challenge for pasting into ChineseForums.com
-  """
-  from challenge import generate_challenge
-  generate_challenge(url)
+    """
+    Generate challenge for pasting into ChineseForums.com
+    """
+    from challenge import generate_challenge
+    generate_challenge(url)
 
 
 @task
 def clean(ctx):
-  """
-  Clean files
-  """
-  from challenge import challenge_file, clip_file
-  for file_ in (challenge_file, clip_file):
-    if file_.exists():
-      os.remove(file_)
+    """
+    Clean files
+    """
+    from challenge import challenge_file, clip_file
+    for file_ in (challenge_file, clip_file):
+        if file_.exists():
+            os.remove(file_)
 
-  for file_ in Path('.').glob('*.csv'):
-    os.remove(file_)
+    for file_ in Path('.').glob('*.csv'):
+        os.remove(file_)
 
 
 @task
 def csv(ctx, url):
-  """
-  Generate lyrics CSV file for import into Notion
-  """
-  from generate import generate_lyrics_csv_file
-  generate_lyrics_csv_file(url)
+    """
+    Generate translation CSV file for import into Notion
+    """
+    from generate import generate_lyrics_csv_file
+    generate_lyrics_csv_file(url)
 
 
 @task
 def lyrics(ctx, title):
-  """
-  Add lyrics into Notion
-  """
-  from challenge import add_lyrics
-  add_lyrics(title)
+    """
+    Add lyrics into Notion
+    """
+    from challenge import add_lyrics
+    add_lyrics(title)
