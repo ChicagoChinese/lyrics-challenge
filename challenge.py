c456f7cd3dc348e0de05c6695364530acef6030e
Feihong Hsu <feihong.hsu@gmail.com>
2019-10-30 10:47:55 -0500

    Add lyrics task extracts lyrics from mp3 files as well

--- challenge.py
+++ challenge.py
@@ -23,157 +23,162 @@ clip_file = here / 'clip.mp3'
 
 
 def generate_challenge(url):
-  page = client.get_block(url)
-  view = client.get_collection_view(url, collection=page.collection)
+    page = client.get_block(url)
+    view = client.get_collection_view(url, collection=page.collection)
 
-  track = get_track(page.title)
-  if not track:
-    print(f'Could not find {track}')
-    return
+    track = get_track(page.title)
+    if not track:
+        print(f'Could not find {track}')
+        return
 
-  print(f'Processing {track}\n')
-  meta = get_page_meta(page)
-  create_audio_clip(track, meta['clip_range'])
-  generate_challenge_file(view, meta)
+    print(f'Processing {track}\n')
+    meta = get_page_meta(page)
+    create_audio_clip(track, meta['clip_range'])
+    generate_challenge_file(view, meta)
 
 
 def add_lyrics(title):
-  track = get_track(title)
-  if track is None:
-    return
+    track = get_track(title)
+    if track is None:
+        return
 
-  print(track)
+    print(track)
 
-  root = client.get_block(settings.LYRICS_ROOT)
+    root = client.get_block(settings.LYRICS_ROOT)
 
-  meta = get_track_meta(track)
-  lyrics = meta.get('lyrics', '').split('\r')
+    meta = get_track_meta(track)
+    lyrics = meta.get('lyrics', '')
+    if lyrics == '':
+        lyrics = meta.get('lyrics-eng', '')
+    lyrics = lyrics.split('\r')
 
-  child = root.children.add_new(PageBlock, title=meta['title'])
-  for line in lyrics:
-    child.children.add_new(TextBlock, title=line)
-    print(line)
+    child = root.children.add_new(PageBlock, title=meta['title'])
+    for line in lyrics:
+        child.children.add_new(TextBlock, title=line)
+        print(line)
+
+    print(f'Lyrics published to {child.get_browseable_url()}')
 
 
 def get_track(title):
-  tracks = list(music_dir.glob(f'**/*{title}*.m4a'))
-  tracks += list(music_dir.glob(f'**/*{title}*.mp3'))
+    tracks = list(music_dir.glob(f'**/*{title}*.m4a'))
+    tracks += list(music_dir.glob(f'**/*{title}*.mp3'))
 
-  if len(tracks) == 0:
-    print('No tracks found')
-    return None
-  elif len(tracks) > 1:
-    print('Found more than one track:')
-    for i, track in enumerate(tracks, 1):
-      print(f'{i}. {track}')
-    choice = int(input('Which one do you want? '))
-    return tracks[choice - 1]
-  else:
-    return tracks[0]
+    if len(tracks) == 0:
+        print('No tracks found')
+        return None
+    elif len(tracks) > 1:
+        print('Found more than one track:')
+        for i, track in enumerate(tracks, 1):
+            print(f'{i}. {track}')
+        choice = int(input('Which one do you want? '))
+        return tracks[choice - 1]
+    else:
+        return tracks[0]
 
 
 def get_track_meta(track):
-  cmd = [
-    'ffprobe',
-    '-v', 'quiet',
-    str(track),
-    '-show_format',
-    '-print_format', 'json',
-  ]
-  meta = json.loads(subprocess.check_output(cmd))
-  return meta['format']['tags']
+    cmd = [
+        'ffprobe',
+        '-v', 'quiet',
+        str(track),
+        '-show_format',
+        '-print_format', 'json',
+    ]
+    meta = json.loads(subprocess.check_output(cmd))
+    return meta['format']['tags']
 
 
 def get_page_meta(page):
-  lines = page.description.splitlines()
-  meta = {}
-  for line in lines:
-    key, value = line.split('=', 1)
-    key = key.strip()
-    meta[key] = value.strip()
+    lines = page.description.splitlines()
+    meta = {}
+    for line in lines:
+        key, value = line.split('=', 1)
+        key = key.strip()
+        meta[key] = value.strip()
 
-  meta['title'] = page.title
-  meta['url'] = page.get_browseable_url()
-  return meta
+    meta['title'] = page.title
+    meta['url'] = page.get_browseable_url()
+    return meta
 
 
 def markdown(text):
-  # Strip off <p></p>:
-  return markdown2.markdown(text)[3:-5]
+    # Strip off <p></p>:
+    return markdown2.markdown(text)[3:-5]
 
 
 def generate_challenge_file(view, meta):
-  meta['link'] = markdown(meta['link'])
+    meta['link'] = markdown(meta['link'])
 
-  chinese_lyrics = []
-  english_lyrics = []
+    chinese_lyrics = []
+    english_lyrics = []
 
-  translation_map = {'': ''}
-  english_lyrics = []
+    translation_map = {'': ''}
+    english_lyrics = []
 
-  for row in view.default_query().execute():
-    chinese_lyrics.append(row.chinese)
+    for row in view.default_query().execute():
+        chinese_lyrics.append(row.chinese)
 
-    if row.english:
-      translation_map[row.chinese] = row.english
+        if row.english:
+            translation_map[row.chinese] = row.english
 
-    line = row.english if row.english != '' \
-      else translation_map.get(row.chinese, f'not found: "{row.chinese}"')
-    english_lyrics.append(line)
+        line = row.english if row.english != '' \
+            else translation_map.get(row.chinese, f'not found: "{row.chinese}"')
+        english_lyrics.append(line)
 
-  with challenge_file.open('w') as fp:
-    related_works = meta.get('related_works')
-    if related_works is not None:
+    with challenge_file.open('w') as fp:
+        related_works = meta.get('related_works')
+        if related_works is not None:
 
-      related_works = markdown(related_works)
+            related_works = markdown(related_works)
 
-    html = env.get_template('challenge.html').render(
-      song=meta, english_lyrics=english_lyrics, chinese_lyrics=chinese_lyrics,
-      related_works=related_works)
+        html = env.get_template('challenge.html').render(
+            song=meta, english_lyrics=english_lyrics, chinese_lyrics=chinese_lyrics,
+            related_works=related_works)
 
-    fp.write(html)
+        fp.write(html)
 
-    print(f'Generated {challenge_file}')
+        print(f'Generated {challenge_file}')
 
 
 def fetch_song_row(title):
-  index_page = client.get_block(settings.CHALLENGE_TABLE)
+    index_page = client.get_block(settings.CHALLENGE_TABLE)
 
-  matching_rows = [
-    row
-    for row in index_page.collection.get_rows()
-    if row.chinese_title == title]
+    matching_rows = [
+        row
+        for row in index_page.collection.get_rows()
+        if row.chinese_title == title]
 
-  if len(matching_rows) == 0:
-    print(f'No entry found for {title}')
-    return None
-  else:
-    return matching_rows[0]
+    if len(matching_rows) == 0:
+        print(f'No entry found for {title}')
+        return None
+    else:
+        return matching_rows[0]
 
 
 def create_audio_clip(track, clip_range_text):
-  if clip_file.exists():
-    return
+    if clip_file.exists():
+        return
 
-  try:
-    start, stop = clip_range_text.strip().split('-')
-  except Exception as ex:
-    print(f'Invalid clip range: {ex}')
-    return
+    try:
+        start, stop = clip_range_text.strip().split('-')
+    except Exception as ex:
+        print(f'Invalid clip range: {ex}')
+        return
 
-  cmd = [
-    'ffmpeg',
-    '-i', str(track),
-    '-ss', start.strip(),
-    '-to', stop.strip(),
-    '-map_metadata', '-1',    # strip metadata
-    # '-acodec', 'copy',      # can't be used when converting between formats
-    str(clip_file)
-  ]
-  subprocess.call(cmd)
+    cmd = [
+        'ffmpeg',
+        '-i', str(track),
+        '-ss', start.strip(),
+        '-to', stop.strip(),
+        '-map_metadata', '-1',    # strip metadata
+        # '-acodec', 'copy',      # can't be used when converting between formats
+        str(clip_file)
+    ]
+    subprocess.call(cmd)
 
-  print(f'Generated {clip_file}')
+    print(f'Generated {clip_file}')
 
 
 if __name__ == '__main__':
-  main()
+    main()
